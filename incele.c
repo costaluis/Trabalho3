9b9726c86354799fd7dbcd3eb927c52765315b09
luisfernando <luis_oliveira@usp.br>
2018-06-13 23:30:48 -0300

    Conclusão da função incele

--- incele.c
+++ incele.c
@@ -1,7 +1,26 @@
 #include "trab3.h"
 
-void incele(elemento *A){
-	if(A->ant==NULL){
-		
+void eleadd(elemento *A){
+	system("clear");
+	printf("Insira a linha do elemento: ");
+	scanf("%d",A->linha);
+	printf("Insira a coluna do elemento: ");
+	scanf("%d",A->coluna);
+	printf("Insira o valor do elemento: ");
+	scanf("%f",A->valor);
+}
+
+elemento* incele(elemento *A,int i){
+	if(i==0){
+		eleadd(A);
+		return A;
+	}
+	else{
+		elemento *B = A;
+		A->prox = (elemento*) calloc(1,sizeof(elemento));
+		A = A->prox;
+		A->ant = B;
+		eleadd(A);
+		return A;
 	}
 }
