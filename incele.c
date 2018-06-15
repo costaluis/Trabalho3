1bbd07f29c5598bae1de20f4eb92341689bb533f
luisfernando <luis_oliveira@usp.br>
2018-06-14 22:29:24 -0300

    Verificação no incele

--- incele.c
+++ incele.c
@@ -1,18 +1,33 @@
 #include "trab3.h"
 
-void eleadd(elemento *A){
+void eleadd(elemento *A,int maxlinha,int maxcoluna){
 	system("clear");
-	printf("Insira a linha do elemento: ");
-	scanf("%d",A->linha);
-	printf("Insira a coluna do elemento: ");
-	scanf("%d",A->coluna);
+	while(1){
+		printf("Insira a linha do elemento: ");
+		scanf("%d",A->linha);
+		if(A->linha>=0 && A->linha<maxlinha){
+			break;
+		}else{
+			printf("Valor Invalido!\n");
+		}
+	}
+	system("clear");
+	while(1){
+		printf("Insira a coluna do elemento: ");
+		scanf("%d",A->coluna);
+		if(A->coluna>=0 && A->coluna<maxcoluna){
+			break;
+		}else{
+			printf("Valor Invalido!\n");
+		}
+	}	
 	printf("Insira o valor do elemento: ");
 	scanf("%f",A->valor);
 }
 
-elemento* incele(elemento *A,int *i){
+elemento* incele(elemento *A,int *i,int maxlinha,int maxcoluna){
 	if(*i==0){
-		eleadd(A);
+		eleadd(A,maxlinha,maxcoluna);
 		*i=1;
 		return A;
 	}
@@ -21,7 +36,7 @@ elemento* incele(elemento *A,int *i){
 		A->prox = (elemento*) calloc(1,sizeof(elemento));
 		A = A->prox;
 		A->ant = B;
-		eleadd(A);
+		eleadd(A,maxlinha,maxcoluna);
 		return A;
 	}
 }
